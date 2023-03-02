5d95b6ec19489f35667320525b81eb6792e7a9e2
eldad mozes <eldad86.m@gmail.com>
2023-03-02 21:00:05 +0200

    examples

--- first_example.py
+++ first_example.py
@@ -1,1 +1,10 @@
-print("Hello world")
+x = 100
+print("x id:", id(x))
+print("x type:", type(x))
+y = 101.1
+print("y id:", id(y))
+print("y type:", type(y))
+z = "eldad"
+print("y id:", id(y))
+print("z type:", type(z))
+print(x, y , z)
